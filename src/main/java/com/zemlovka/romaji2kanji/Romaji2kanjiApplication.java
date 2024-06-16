ecf13ea68b81a4f7e72e936a2499b114edc67f27
Mykhailo Bubnov <applemix00@gmail.com>
2024-06-16 14:26:35 +0200

    Initial commit

---
+++ src/main/java/com/zemlovka/romaji2kanji/Romaji2kanjiApplication.java
@@ -0,0 +1,14 @@
+package com.zemlovka.romaji2kanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Romaji2kanjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Romaji2kanjiApplication.class, args);
+    }
+
+}
